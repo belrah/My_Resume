5c21ae814f24587a11f25259abb61cd110d2bbda
belrah <belrahj3@gmail.com>
2021-08-18 15:47:31 +0100

    used javascript to write my full name

---
+++ assets/js/FullName.js
@@ -0,0 +1,7 @@
+const fullName = (name, surname_name) => {
+    return `My name is ${name} ${surname_name}`;
+};
+
+document.write(fullName('Mercy', 'Kalu')); //This should be able to write Mercy Kalu on the page
+
+//copy and paste the above code to check on the console

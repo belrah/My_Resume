23f5e4b4c366f955ea571c7e8cca40711c0fb40b
belrah <belrahj3@gmail.com>
2021-08-18 17:04:21 +0100

    created Javascript code to print my name

--- assets/js/FullName.js
+++ assets/js/FullName.js
@@ -3,5 +3,3 @@ const fullName = (name, surname_name) => {
 };
 
 document.write(fullName('Mercy', 'Kalu')); //This should be able to write Mercy Kalu on the page
-
-//copy and paste the above code to check on the console
